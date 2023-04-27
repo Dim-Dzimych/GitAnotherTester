7312b6e8f96727ac00b2347caaab72af552bc100
Dzmitry Hastsilovich <dzmitryhastsilovich@MacBook-Pro-Dim.local>
2023-04-27 20:14:56 +0300

    Add TGSpy

---
+++ TelegramEarningBot.Api/Controllers/BotController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TelegramEarningBot.Api.Data;
+using TelegramEarningBot.Api.Data.Entities;
+using Task = System.Threading.Tasks.Task;
+
+namespace TelegramEarningBot.Api.Controllers;
+
+[ApiController]
+public class BotController : ControllerBase
+{
+    private readonly EarningBotContext _context;
+
+    public BotController(EarningBotContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("d")]
+    public async Task<IActionResult> ValidateRedirect(long id, long userId)
+    {
+        var sendingPost = _context.SendingPosts.Include(x => x.PostVisitors).FirstOrDefault(x => x.Id == id);
+
+        if (sendingPost == null)
+        {
+            return BadRequest();
+        }
+        
+        sendingPost.PostVisitors?.Add(new PostVisitor
+        {
+            SendingPostId = id,
+            UserId = userId
+        });
+        
+        await _context.SaveChangesAsync();
+
+        //await Task.Delay(-1);
+
+        return Redirect(sendingPost.Link!);
+       
+    }
+
+    [HttpGet("spystat")]
+    public async Task<IActionResult> LinkList()
+    {
+
+        var visits = _context.SendingPosts
+            .GroupBy(x => x.Link)
+            .Select(x => new {link = x.Key, count = x.Count()})
+            .ToList();
+        return Ok(visits);
+    }
+}
